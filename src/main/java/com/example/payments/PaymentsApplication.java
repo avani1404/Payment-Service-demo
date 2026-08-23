09cd72f3cf86d16f1dbb76dc8da8e2e4bef54459
avanimaheshwari <avanijakhetiya@gmail.com>
2026-08-23 21:56:44 +0530

    dummy commit

--- src/main/java/com/example/payments/PaymentsApplication.java
+++ src/main/java/com/example/payments/PaymentsApplication.java
@@ -12,7 +12,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class PaymentsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PaymentsApplication.class, args);
     }
